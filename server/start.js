b91ae0543f6dd969bd5ca6819e1f1332b6ebb4b7
Shane Tomlinson <stomlinson@mozilla.com>
2013-05-12 14:44:02 -0700

    initial commit.

---
+++ server/start.js
@@ -0,0 +1,39 @@
+const express         = require('express');
+const cons            = require('consolidate');
+const swig            = require('swig');
+const path            = require('path');
+const connect_fonts   = require('connect-fonts');
+const fontpack_opensans
+                      = require('connect-fonts-opensans');
+const app             = express();
+
+const PORT            = process.env.PORT || 3000;
+
+const TEMPLATE_PATH   = path.join(__dirname, 'views');
+const STATIC_PATH     = path.join(__dirname, '..', 'client');
+
+app.engine('.html', cons.swig);
+app.set('view engine', 'html');
+
+swig.init({
+  root: TEMPLATE_PATH,
+  allowErrors: true
+});
+
+
+app.set('views', TEMPLATE_PATH);
+
+app.use(connect_fonts.setup({
+  fonts: [ fontpack_opensans ],
+  ua: 'all'
+}));
+
+app.use(express.static(STATIC_PATH));
+
+app.get('/', function (req, res) {
+  res.render('index.html', { foo: 'bar' });
+});
+
+console.log("Listening on port", PORT);
+app.listen(PORT);
+
